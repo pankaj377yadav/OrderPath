5548944d42b7313dc67aa2171538c0aa37a454bd
Pankaj Kumar Yadav <pankaj377yadav@gmail.com>
2023-08-12 19:51:26 +0545

    Making connection between client and server

--- client/src/pages/register/index.js
+++ client/src/pages/register/index.js
@@ -1,10 +1,12 @@
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
+import {useState} from 'react'
 import Link from 'next/link';
 import styles from "@/styles/form.module.css"
 
 
 const Register = ( )=> {
+  const [role, setRole] = useState("user")
    
     return (
         <div className={styles.body}>
@@ -15,6 +17,7 @@ const Register = ( )=> {
             fullName: '',
             phoneNumber: '',
             password: '',
+            confirmPassword: '',
             email: ''
           }}
           onSubmit={values => {
@@ -41,21 +44,27 @@ const Register = ( )=> {
                 <div>{errors.phoneNumber}</div>
               ) : null}
               <br/>
+              <Field name="email"  placeholder="Email" type="email" className={styles.input}/>
+              {errors.email && touched.email ? <div>{errors.email}</div> : null}
+              <br/>
               <Field name="password" placeholder="Password" type="text" className={styles.input}/>
               {errors.password && touched.password? (
                 <div>{errors.password}</div>
               ) : null}
               <br/>
-              <Field name="email"  placeholder="Email" type="email" className={styles.input}/>
-              {errors.email && touched.email ? <div>{errors.email}</div> : null}
+              <Field name="confirmPassword" placeholder="Confirm Password" type="text" className={styles.input}/>
+              {errors.confirmPassword && touched.confirmPassword? (
+                <div>{errors.confirmPassword}</div>
+              ) : null}
               <br/>
               <Field as="select" name="role" className={styles.input}>
-                <option value="User">User</option>
-                <option value="Rider">Rider</option>
+                <option value="User" onClick={()=>setRole("user")}>User</option>
+                <option value="Rider" onClick={()=>setRole("rider")}>Rider</option>
+                
                 </Field>
                 <br/>
               <button type="submit"  className={styles.submit}>Submit</button>
-              Already User!
+              Already User! {role}
               <br/> <Link href="/login" className={styles.submit}>Sign in</Link>
             </Form>
           )}
