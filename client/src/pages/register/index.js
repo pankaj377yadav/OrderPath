49eb39269bdae9ed0f8cddd3d16bbe5cecbc8e15
Pankaj Kumar Yadav <pankaj377yadav@gmail.com>
2023-08-12 10:49:07 +0545

    added CSS in login and Register page

--- client/src/pages/register/index.js
+++ client/src/pages/register/index.js
@@ -1,16 +1,18 @@
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
-import Link from 'next/link'
+import Link from 'next/link';
+import styles from "@/styles/form.module.css"
 
 
 const Register = ( )=> {
    
     return (
-        <div>
+        <div className={styles.body}>
     
       
         <Formik
           initialValues={{
+            fullName: '',
             phoneNumber: '',
             password: '',
             email: ''
@@ -26,23 +28,35 @@ const Register = ( )=> {
           }}
         >
           {({ errors, touched }) => (
-            <Form>
-              <Field name="phoneNumber" placeholder="phoneNumber"/>
+              <Form className={styles.form}>
+              <h2 className={styles.title}>Register</h2>
+
+
+              <Field name="fullName" placeholder="Full Name" type="text" className={styles.input}/>
+              {errors.fullName && touched.fullName ? (
+                <div>{errors.phoneNumber}</div>
+              ) : null}<br/>
+              <Field name="phoneNumber" placeholder="Phone Number" type="number" className={styles.input}/>
               {errors.phoneNumber && touched.phoneNumber ? (
                 <div>{errors.phoneNumber}</div>
               ) : null}
               <br/>
-              <Field name="password" placeholder="password"/>
+              <Field name="password" placeholder="Password" type="text" className={styles.input}/>
               {errors.password && touched.password? (
                 <div>{errors.password}</div>
               ) : null}
               <br/>
-              <Field name="email"  placeholder="email"/>
+              <Field name="email"  placeholder="Email" type="email" className={styles.input}/>
               {errors.email && touched.email ? <div>{errors.email}</div> : null}
               <br/>
-              
-              <button type="submit">Submit</button>
-              Already User <Link href="/login">Sign in</Link>
+              <Field as="select" name="role" className={styles.input}>
+                <option value="User">User</option>
+                <option value="Rider">Rider</option>
+                </Field>
+                <br/>
+              <button type="submit"  className={styles.submit}>Submit</button>
+              Already User!
+              <br/> <Link href="/login" className={styles.submit}>Sign in</Link>
             </Form>
           )}
         </Formik>
